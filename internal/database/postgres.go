bf4b974cd01cb492c1939374dd71fd0e2b06825d
codepnw <idchokms01@outlook.com>
2025-06-19 21:08:45 +0700

    database: add transaction support

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
@@ -21,4 +22,37 @@ func InitPostgresDB(addr string) (*sql.DB, error) {
 	log.Println("database connected...")
 
 	return db, err
+}
+
+type TxManager interface {
+	WithTx(ctx context.Context, fn func(tx *sql.Tx) error) error
+}
+
+type sqlTxManager struct {
+	db *sql.DB
+}
+
+func NewSqlTxManager(db *sql.DB) TxManager {
+	return &sqlTxManager{db: db}
+}
+
+func (m *sqlTxManager) WithTx(ctx context.Context, fn func(tx *sql.Tx) error) (err error) {
+	tx, err := m.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+			panic(p)
+		} else if err != nil {
+			tx.Rollback()
+		} else {
+			err = tx.Commit()
+		}
+	}()
+
+	err = fn(tx)
+	return err
 }
